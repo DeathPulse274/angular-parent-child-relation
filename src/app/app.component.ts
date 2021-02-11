2f56145379174e5db4e8d17a3155ed3fa285aefb
Santhosh K <santhosh.k@corp.rapyder.com>
2021-02-11 14:02:35 +0530

    Initial COmmit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{ type: "blueprint", content: "Some Data", name: "some Name" }, { type: "server", content: "Some Data", name: "some Name" }];
+
+  onAddServer(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onAddBlueprint(bluePrintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
+
+}
